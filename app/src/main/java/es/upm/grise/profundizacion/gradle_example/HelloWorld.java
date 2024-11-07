ed02d5e2668b4b833005bfe445b1406ded2fc827
odieste <odieste@fi.upm.es>
2024-11-07 17:59:26 +0100

    Project added

---
+++ app/src/main/java/es/upm/grise/profundizacion/gradle_example/HelloWorld.java
@@ -0,0 +1,9 @@
+package es.upm.grise.profundizacion.gradle_example;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("The parameter has the value: " + System.getProperty("parameter"));
+	}
+
+}
---
+++ app/src/test/java/es/upm/grise/profundizacion/gradle_example/HelloWorldTest.java
@@ -0,0 +1,14 @@
+package es.upm.grise.profundizacion.gradle_example;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+class HelloWorldTest {
+
+	@Test
+	void test() {
+		assertTrue(true);
+	}
+
+}
